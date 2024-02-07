aeb92c4914c71f28a4eff4fe738425c646575b14
mohand-djouadi <mohand.djouadi@fgei.ummto.dz>
2024-02-07 13:47:52 +0100

    styling task-list define task-det

--- src/app/task-det/task-det.component.ts
+++ src/app/task-det/task-det.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { task } from '../models/task.model';
 
 @Component({
   selector: 'app-task-det',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './task-det.component.css'
 })
 export class TaskDetComponent {
+
+  @Input() task!: task;
 
 }
