abd4a123dd5031051f1fe60a44af061c02f5ac24
mohand-djouadi <mohand.djouadi@fgei.ummto.dz>
2024-10-10 16:23:47 +0200

    add task router-outlet

--- src/app/task/task-routing.module.ts
+++ src/app/task/task-routing.module.ts
@@ -3,11 +3,17 @@ import {RouterModule, Routes} from "@angular/router";
 import {TasksComponent} from "./pages/tasks/tasks.component";
 import {NewTaskComponent} from "./pages/new-task/new-task.component";
 import {SettingsComponent} from "./pages/settings/settings.component";
+import {TaskMainComponent} from "./task-main.component";
 
 const routes: Routes = [
-  {path: '', component: TasksComponent},
-  { path: 'add', component: NewTaskComponent },
-  { path: 'settings', component: SettingsComponent }
+  { path: '', component: TaskMainComponent,
+    children: [
+      { path: '', component: TasksComponent },
+      { path: 'add', component: NewTaskComponent },
+      { path: 'settings', component: SettingsComponent }
+    ]
+  },
+
 ]
 
 @NgModule({
